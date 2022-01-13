a29a958b8a9085a944859def3d499526056cf850
Stas Pasynkov <s.pasynkov@gmail.com>
2022-01-13 03:21:30 +0200

    Initial commit

---
+++ src/main/java/com/example/budgetlogger/BudgetLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgetlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/budgetlogger/BudgetLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budgetlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
